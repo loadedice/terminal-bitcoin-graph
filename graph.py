0148ed6d66bb4580da533f82758b48dc61b1e6b3
joelina <joelina@users.noreply.github.com>
2014-01-11 17:39:11 +1100

    make x axis show the time

--- graph.py
+++ graph.py
@@ -2,21 +2,21 @@ import termsiz, subprocess, json, time
 from urllib import request
 x,y = [],[]
 api = "https://btc-e.com/api/2/btc_usd/ticker"
-queue = 10
+queue = 50
+gnuplot = subprocess.Popen(["/usr/bin/gnuplot"],stdin=subprocess.PIPE)
 
 while not False:
     req = request.urlopen(api)
     encoding = req.headers.get_content_charset()
     data = json.loads(req.read().decode(encoding))
-    x.append(data['ticker']['updated'])
+    #x.append(data['ticker']['updated'])
+    x.append(time.strftime("%X"))
     y.append(data['ticker']['last'])
-    """"""
-    gnuplot = subprocess.Popen(["/usr/bin/gnuplot"],stdin=subprocess.PIPE)
-    settings="set term dumb {} {}\n".format(termsiz.get_terminal_width(),termsiz.get_terminal_height())
+    settings="set term dumb {} {} ;set xdata time; set timefmt \"%H:%M:%S\"\n".format(termsiz.get_terminal_width(),termsiz.get_terminal_height())
     gnuplot.stdin.write(bytes(settings,"utf-8"))
     gnuplot.stdin.write(bytes("plot '-' using 1:2 title 'Bitcoin' with linespoints \n","utf-8"))
     for i,j in zip(x,y):
-        gnuplot.stdin.write(bytes("%f %f\n" % (i,j),"utf-8"))
+        gnuplot.stdin.write(bytes("%s %f\n" % (i,j),"utf-8"))
     gnuplot.stdin.write(bytes("e\n","utf-8"))
     gnuplot.stdin.flush()
-    time.sleep(12)
+    time.sleep(10)
